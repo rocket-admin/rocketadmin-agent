414c7f88fd0a3342de129dffda06dd2685468fe8
Artuom <r.tem@tuta.io>
2021-08-17 19:06:47 +0300

    added message in testConnection methods

--- src/helpers/check-connection.ts
+++ src/helpers/check-connection.ts
@@ -1,9 +1,8 @@
 import { IConnection } from '../interfaces/interfaces';
 import { createDao } from '../dal/shared/create-dao';
+import { ITestConnectResult } from '../dal/shared/dao-interface';
 
-export async function checkConnection(
-  connection: IConnection,
-): Promise<boolean> {
+export async function checkConnection(connection: IConnection): Promise<ITestConnectResult> {
   console.log('-> Test connection to database');
   const dao = createDao(connection);
   const result = await dao.testConnect();
